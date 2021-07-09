5072297767df5d59622beb73a81688625f4c7c14
alequinterok <aquintero@fi.uba.ar>
2021-07-09 15:44:28 -0300

    Se modifico el main

--- menu.py
+++ menu.py
@@ -9,34 +9,35 @@ def validar_opcion(minimo: int, maximo: int) -> int:
 
 
 def main() -> None:
-    print(
-""" 
-1) Listar archivos de la carpeta actual.
-2) Crear un archivo.
-3) Subir un archivo.
-4) Descargar un archivo.
-5) Sincronizar.
-6) Generar carpetas de una evaluacion.
-7) Actualizar entregas de alumnos vía mail.
-8) Salir. 
-""")
-    
-    opcion = validar_opcion(1, 8)
-    print(opcion)
-    if opcion == 1:
-        pass
-    elif opcion == 2:
-        pass
-    elif opcion == 3:
-        pass
-    elif opcion == 4:
-        pass
-    elif opcion == 5:
-        pass    
-    elif opcion == 6:
-        pass
-    elif opcion == 7:
-        pass
-    elif opcion == 8:
-        pass   
+    continuar = True
+    while continuar:
+        print(
+            """ 
+            1) Listar archivos de la carpeta actual.
+            2) Crear un archivo.
+            3) Subir un archivo.
+            4) Descargar un archivo.
+            5) Sincronizar.
+            6) Generar carpetas de una evaluacion.
+            7) Actualizar entregas de alumnos vía mail.
+            8) Salir. 
+            """)
+        
+        opcion = validar_opcion(1, 8)
+        if opcion == 1:
+            pass
+        elif opcion == 2:
+            pass
+        elif opcion == 3:
+            pass
+        elif opcion == 4:
+            pass
+        elif opcion == 5:
+            pass    
+        elif opcion == 6:
+            pass
+        elif opcion == 7:
+            pass
+        elif opcion == 8:
+            continuar = False 
 main()
