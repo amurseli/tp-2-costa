111688816b36f33073ebe51ad1ac79dbdb33c943
amurseli <amurseli@fi.uba.ar>
2021-07-13 17:43:39 -0300

    Eliminación de archivo de más y correción de error menor

--- drive.py
+++ drive.py
@@ -10,29 +10,29 @@ folder_id = "1jjV0gKN_gM7aSYWCsqdLnIX_wa_qBSxt"
 file_names = ["Prueba.txt", "Esto es otra Prueba.rar"]
 mime_types = ["text/plain","application/vnd.rar"]
 
-for file_name, mime_type in zip(file_names, mime_types):
-    file_metadata = {
-        "name" : file_name,
-        "parents" : [folder_id]
-
-    }
-
-    media = MediaFileUpload("C:\\Users\\Matias\\Desktop\\{0}".format(file_name), mimetype=mime_type)
-
-    service.files().create(
-        body = file_metadata,
-        media_body = media,
-        fields = "id"
-    ).execute()
-
-# Hacer Carpetas Vacías
-# group = ['agustin', 'nehuen', 'ingnacio', 'alejandro']
-
-# for participant in group:
+# for file_name, mime_type in zip(file_names, mime_types):
 #     file_metadata = {
-#         'name': participant,
-#         'mimeType': 'application/vnd.google-apps.folder'
-#         #"parents": []
+#         "name" : file_name,
+#         "parents" : [folder_id]
+
 #     }
 
-# service.files().create(body=file_metadata).execute()
+#     media = MediaFileUpload("C:\\Users\\Matias\\Desktop\\{0}".format(file_name), mimetype=mime_type)
+
+#     service.files().create(
+#         body = file_metadata,
+#         media_body = media,
+#         fields = "id"
+#     ).execute()
+
+#Hacer Carpetas Vacías
+group = ['agustin', 'nehuen', 'ingnacio', 'alejandro']
+
+for participant in group:
+    file_metadata = {
+        'name': participant,
+        'mimeType': 'application/vnd.google-apps.folder'
+        #"parents": []
+    }
+
+service.files().create(body=file_metadata).execute()
