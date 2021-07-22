cd25a4becb805e9ad74391766715b5915d5d7f97
alequinterok <aquintero@fi.uba.ar>
2021-07-22 13:12:03 -0300

    se modifico sistema de carpeta

--- sistema_carpeta.py
+++ sistema_carpeta.py
@@ -1,11 +1,12 @@
 import os
 from service_drive import obtener_servicio_drive
 
-evaluacion = os.mkdir("Carpeta_Prueba")
-docente = os.mkdir("Carpeta_Prueba/Docentes")
-alumnos = os.mkdir("Carpeta_Prueba/Docentes/Alumnos")
-with open("Carpeta_Prueba/Docentes/Alumnos/Archivo.txt", "w") as files:
-    elemento = files
+def sistema_carpeta():
+    evaluacion = os.mkdir("Carpeta_Prueba")
+    docente = os.mkdir("Carpeta_Prueba/Docentes")
+    alumnos = os.mkdir("Carpeta_Prueba/Docentes/Alumnos")
+    with open("Carpeta_Prueba/Docentes/Alumnos/Archivo.txt", "w") as files:
+        elemento = files
 
 service = obtener_servicio_drive()
 
