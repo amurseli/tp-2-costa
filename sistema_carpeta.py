86a74aff5aa2f8a4fd06734bf3bd3a2c3cd56903
alequinterok <aquintero@fi.uba.ar>
2021-07-22 20:09:55 -0300

    se corrigio el sistema de carpetas y crea en local

--- sistema_carpeta.py
+++ sistema_carpeta.py
@@ -1,18 +1,11 @@
 import os
-from service_drive import obtener_servicio_drive
-from service_gmail import obtener_servicio_gmail
-from mandar_mensaje import enviar_mensaje
-from mandar_mensaje import enviar_mensaje_con_adjuntos
-
 
 def sistema_carpeta():
-    evaluacion = os.mkdir("Carpeta_Prueba")
-    docente = os.mkdir("Carpeta_Prueba/Docentes")
-    alumnos = os.mkdir("Carpeta_Prueba/Docentes/Alumnos")
-    with open("Carpeta_Prueba/Docentes/Alumnos/Archivo.txt", "w") as files:
+    evaluacion = os.mkdir("Evaluacion")
+    docente = os.mkdir("Evaluacion/Docentes")
+    alumnos = os.mkdir("Evaluacion/Docentes/Alumnos")
+    with open("Evaluacion/Docentes/Alumnos/Archivo.txt", "w") as files:
         elemento = files
-
-service = obtener_servicio_drive()
-
-from subir_archivo import subir_archivos
-subir_archivos(service)
+        
+""" sistema_carpeta()
+print("Se creo el sistema de carpetas en local") """
