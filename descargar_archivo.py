90dc2fbbd70252c5ff85657b77d8f4026fa4a48c
amurseli <amurseli@fi.uba.ar>
2021-07-26 09:56:35 -0300

    Acortado el modulo de Descargas

--- descargar_archivo.py
+++ descargar_archivo.py
@@ -5,6 +5,35 @@ from googleapiclient.http import MediaIoBaseDownload
 from seleccionar_carpeta import seleccionar_carpeta
 
 def descargar_archivos(service) -> None:
+    file_id, file_name = seleccionar_archivo(service)
+    request = service.files().get_media(fileId=file_id)
+    fh = io.BytesIO()
+        
+    downloader = MediaIoBaseDownload(fh, request, chunksize=204800)
+    done = False
+
+    print("Seleccione una Carpeta en la ventan que se acaba de abrir. Es posible que no se haya abierto en primer plano.")
+    path = askdirectory(title='Seleccione una carpeta') 
+
+    try:
+        # Descarga la info "Chunk por Chunk"
+        while not done:
+            status, done = downloader.next_chunk()
+
+        fh.seek(0)
+
+        with open(f"{path}\\{file_name}", 'wb') as f:
+            f.write(fh.read())                       #Reescribe el archivo creado en al linea anterior con la ingormación en fh
+
+        print("Archivo Descargado con éxito!")
+    except :
+    
+        print("Algo salió Mal.")
+        print("Es probable que estes intentando descargar un archivo no descargable.")
+        print("Algunos de los archivos no descargables son los que fueron editados por las aplicaciones de drive como 'Google Docs' o 'Goggle Sheets'")
+        return False
+
+def seleccionar_archivo(service) -> tuple:
     file_id = ""
     folder_id, flag_root = seleccionar_carpeta(service)
     if flag_root == False:
@@ -33,30 +62,4 @@ def descargar_archivos(service) -> None:
                 if element[key] == file_name:
                     target_file = element
                     file_id = target_file["id"] 
-
-    request = service.files().get_media(fileId=file_id)
-    fh = io.BytesIO()
-        
-    downloader = MediaIoBaseDownload(fh, request, chunksize=204800)
-    done = False
-
-    print("Seleccione una Carpeta en la ventan que se acaba de abrir. Es posible que no se haya abierto en primer plano.")
-    path = askdirectory(title='Seleccione una carpeta') 
-
-    try:
-        # Descarga la info "Chunk por Chunk"
-        while not done:
-            status, done = downloader.next_chunk()
-
-        fh.seek(0)
-
-        with open(f"{path}\\{file_name}", 'wb') as f:
-            f.write(fh.read())                       #Reescribe el archivo creado en al linea anterior con la ingormación en fh
-
-        print("Archivo Descargado con éxito!")
-    except :
-    
-        print("Algo salió Mal.")
-        print("Es probable que estes intentando descargar un archivo no descargable.")
-        print("Algunos de los archivos no descargables son los que fueron editados por las aplicaciones de drive como 'Google Docs' o 'Goggle Sheets'")
-        return False
+    return (file_id,file_name)
